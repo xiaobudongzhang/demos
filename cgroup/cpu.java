92c67325585a1366cc6781e616657710d2128c83
xiaobudongzhang <xiaobudongzhang@gmail.com>
2017-01-19 14:13:33 +0800

    add cgroup about cpu

---
+++ cgroup/cpu.java
@@ -0,0 +1,31 @@
+public class HoldCPUMain{
+    public static class HoldCPUTask implements Runnable{
+	@Override
+	public void run(){
+	    while(true){
+		double a=Math.random()*Math.random();//use cpu
+		System.out.println(a);
+	    }
+	}
+    }
+    
+    public static class LazyTask implements Runnable{
+	public void run(){
+	    while(true){
+		try{
+		    Thread.sleep(1000);
+		}catch(InterruptedException e){
+		    e.printStackTrace();
+		}
+	    }
+	}
+    }
+    
+    
+    public static void main(String[] arg){
+	for(int i=0;i<10;i++){
+	    new Thread(new HoldCPUTask().start());
+	}
+    }
+}
+
