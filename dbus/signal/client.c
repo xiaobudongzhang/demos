b855eefacd550d9940dabd1e7a6e9c8a081a087c
xiaobudongzhang <xiaobudongzhang@gmail.com>
2017-03-08 14:41:41 +0800

    add webhook test

---
+++ dbus/signal/client.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dbus-1.0/dbus/dbus-glib.h>
+#include <dbus-1.0/dbus/dbus.h>
+
+int send_sig(char *sigvalue){
+
+}
+
+int main(int argc,char **argv){
+  return 0;
+}
